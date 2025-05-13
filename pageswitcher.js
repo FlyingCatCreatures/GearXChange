f19f299fce122b94ec9dcd8a4d665f71e16c266c
FlyingCatCreatures <harstaanne@gmail.com>
2025-05-13 20:05:07 +0200

    Added pages that were in the top bar

--- pageswitcher.js
+++ pageswitcher.js
@@ -1,19 +1,13 @@
 document.addEventListener('DOMContentLoaded', () => {
-    console.log('DOMContentLoaded fired');
-    const button = document.getElementById('page-switcher-button');
+    const buttons = document.querySelectorAll('.page-switcher-button');
 
     window.api.on('window-ready', () => {
-        console.log('window-ready event received');
 
-        if (button) {
-            console.log('Button found, adding click listener');
+        buttons.forEach(button => {
             button.addEventListener('click', () => {
                 const target = button.getAttribute('data-target');
-                console.log('button clicked with target ' + target);
                 window.api.gotoPage(target);
             });
-        } else {
-            console.log('Button not found!');
-        }
+        });
     });
 });
